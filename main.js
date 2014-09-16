16b6ee248e90ba6b6107fddab0eafebf78058c96
Adam Weiner <adamwineguy@gmail.com>
2014-09-16 16:27:31 -0600

    updated jquery work

---
+++ main.js
@@ -0,0 +1,23 @@
+// var liItems = $('li')
+// 	.css("color", "red")
+// 	.text("Changed from jQuery")
+// 	.width(50);
+
+// var newElement = $("<li>From jQuery</li>");
+
+// newElement.css("color", "red");
+
+// $("#my-list").append(newElement);
+
+
+
+$("#fancy").click(function() {
+	var newElement=$("<li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li>");
+	   
+		newElement.css("color", "#cb04d6");
+	$("#my-list").append(newElement);
+$(".main-container").hide(5000);
+
+	console.log("Don't touch my fucking my button!");
+alert("Don't touch my fucking button!");
+});

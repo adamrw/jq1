c413dc5cb05d4fffa71109588f87ee7f7a92119b
Adam Weiner <adamwineguy@gmail.com>
2014-09-16 20:56:52 -0600

    jQuery part 1 completed

--- main.js
+++ main.js
@@ -11,13 +11,24 @@
 
 
 
-$("#fancy").click(function() {
-	var newElement=$("<li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li> ");
+$("#first-button").click(function() {
+	var newP1=$("<p>Here's some additional text for ya, asshole!</p>");
+		newP1.css("color", "#cb04d6");
+	$("#first-button").append(newP1);
+$(".main-container").hide(10000);
+$(".main-container").show(5000);
+console.log("Don't touch my fucking button!");
+alert("Now watch what happens... you'll know why I'm fucking angry at my buttons!");
+});
+$("#second-button").click(function() {
+	var newH1 = $("<h1>Dick! You Added A Header In This Button!</h1>");
+	newH1.css("color", "#31708F");
+	$("#second-button").append(newH1);
+});
+
+$("#third-button").click(function() {
+	var newElement=$("<li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li><li>Holy Shit!</li>");
 	   
 		newElement.css("color", "#cb04d6");
-	$("#my-list").append(newElement);
-$(".main-container").hide(5000);
-
-	console.log("Don't touch my fucking my button!");
-alert("Don't touch my fucking button!");
+	$("#third-button").append(newElement);
 });
